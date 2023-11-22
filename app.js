3d3a38c9b07febf7fdf631ed22741e2b19c98130
masengeshocadeau <masengeshocadeau603@gmail.com>
2023-11-22 21:32:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let currentMusic = 0;
+
+const music = document.querySelector('#audio');
+const seekBar = document.querySelector('.seek-bar');
+const songName = document.querySelector('.music-name');
+const artistName = document.querySelector('.artist-name');
+const disk = document.querySelector('.disk');
+const currentTime = document.querySelector('.current-time');
+const musicDuration = document.querySelector('.song-duration');
+
+const playBtn = document.querySelector('.play-btn');
+const forwardBtn = document.querySelector('.forward-btn');
+const backwardBtn = document.querySelector('.backward-btn');
+
+playBtn.addEventListener('click', ()=>{
+    if(playBtn.className.includes('pause')){
+        music.play();
+    } else{
+        music.pause();
+    }
+    playBtn.classList.toggle('pause');
+    disk.classList.toggle('play');
+})
+
+// setup music
+const setMusic = (i)=>{
+    seekBar.value = 0; //set range slide value to 0;
+    let song = songs[i];
+    music.src = song.path;
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImag = `url('${song.cover}')`;
+    currentTime.innerHTML = '00:00';
+    setTimeout(()=>{
+        seekBar.max = music.duration
+    musicDuration.innerHTML = formatTime(music.duration);
+    },300);
+
+}
+
+setMusic(0)
+
+// formatting time in min and  seconds format
---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/song 1.mp3',
+        artist: 'artist 1',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/song 2.mp3',
+        artist: 'artist 2',
+        cover: 'images/cover 2.png'  
+    },
+    {
+        name: 'song 3',
+        path: 'musics/song 3.mp3',
+        artist: 'artist 3',
+        cover: 'images/cover 3.png'    
+    },
+    {
+        name: 'song 5',
+        path: 'musics/song 5.mp3',
+        artist: 'artist 5',
+        cover: 'images/cover 5.png' 
+    },
+]
